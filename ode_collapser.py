f15762ce47734f57648192cb55e1199cc008063f
Harry Braviner <harry.braviner@gmail.com>
2025-07-06 22:12:43 -0400

    Breakout ODE-collapser into a file

---
+++ ode_collapser.py
@@ -0,0 +1,168 @@
+import torch
+from tqdm import tqdm
+
+def collapse_to_solution(
+        rhs,
+        h,
+        t_start,
+        t_end,
+        idx_samples,
+        x_samples,
+        transformation_x2z=None,
+        N_iter=5000,
+        get_w_ODE=None,
+        logging_freq_scalars=1,
+        logging_freq_grids=10,
+        first_deriv_fwd_mode=True,
+        show_progress=False,
+):
+    # Get the number of grid points
+    t_grid = torch.arange(t_start, t_end, h)
+    n_grid = t_grid.shape[0]
+
+    # If the inputs are not torch tensors, try and make tensors from them.
+    assert idx_samples.shape[0] == x_samples.shape[0]
+    if not isinstance(idx_samples, torch.Tensor):
+        idx_samples = torch.tensor(idx_samples, requires_grad=False)
+    if not isinstance(x_samples, torch.Tensor):
+        x_samples = torch.tensor(x_samples, requires_grad=False)
+
+    # Default transformation
+    if transformation_x2z is None:
+        # The default option transforms the problem into gradient-space plus a constant.
+        transformation_x2z = torch.zeros((n_grid, n_grid), dtype=torch.float64)
+        transformation_x2z[0, 0] = 1.0  # z_0 = x(t_0)
+        for i in range(1, n_grid):
+            # z_i = x(t_i) - x(t_{i-1}) for i > 0
+            transformation_x2z[i, i] = 1.0
+            transformation_x2z[i, i - 1] = -1.0
+
+    # Default loss-weighting schedule
+    if get_w_ODE is None:
+        def get_w_ODE(it, n_iterations):
+            if it < 0.1 * n_iterations:
+                # First 10% of steps: optimize mainly for fitting the samples
+                w_ode = 0.01
+            elif it >= 0.9 * n_iterations:
+                # Final 90% of steps: optimize mainly for satisfying the ODE
+                w_ode = 0.8
+            else:
+                # Linear ramp-up of w_ODE in between these iterations
+                w_ode = 0.01 + 0.79 * (it - 0.1 * n_iterations) / (0.8 * n_iterations)
+            return w_ode
+
+    # Invert the transformation matrix. We will need this repeatedly later.
+    if torch.linalg.det(transformation_x2z).item() < 1e-4:
+        error_msg = f'Transformation matrix from [x(t_i)] to [z_i] has a small determinant.'
+        raise ValueError(error_msg)
+    transformation_z2x = torch.linalg.inv(transformation_x2z)
+
+    # Initialize the solution grid.
+    # I don't believe there is any benefit to using random initialization, since this problem does not
+    # have the same requirement for symmetry-breaking that exists with the hidden neurons of a neural network.
+    z_solution_grid = torch.zeros(n_grid, dtype=torch.float64, requires_grad=True)
+
+    # Initialize the optimizer.
+    # This problem seems to benefit from using a second-order optimizer (which LBFGS is), and I believe that
+    # is due to the Hessian of loss_ODE (see below for definition) having a very large condition number.
+    optimizer = torch.optim.LBFGS(lr=1, history_size=10, params=[z_solution_grid])
+
+    # Loss function definitions
+
+    # The loss for the optimization problem has two parts:
+    # loss_data measures the l2 error of the data relative to our current solution x(t_0), x(t_1), ..., x(t_{N-1})
+    # loss_ODE measures the l2 norm of the local violation of the ODE.
+    # Our aim is to bring loss_ODE to zero while keeping loss_data as small as possible.
+
+    # Note that loss_data is normalized by the number of samples.
+    def loss_data(z):
+        x = transformation_z2x @ z
+        x_at_sample_points = x[idx_samples]
+        error_of_samples = (x_at_sample_points - x_samples)
+        loss_val = 0.5 * torch.mean(error_of_samples ** 2)
+        return loss_val
+
+    # Note that this loss is a sum over only the interior points, 1, 2, ..., N-2.
+    # This is because we do not have sufficient data to compute the second derivative at points 0 and N-1.
+    # This should be consistent with your intuition: if we simply demand that loss_ODE = 0,
+    # we would have N-2 equations in N unknowns.
+    # This would (typically) have a two-dimensional space of solutions,
+    # which is what we should expect for a 2nd order ODE.
+    def loss_ODE(z):
+        x = transformation_z2x @ z
+        # Note the factor of h^-2. As h is increased (i.e. the grid is made finer) this should converge to the value
+        # of the second derivative (so long as x(t) is twice-differentiable).
+        second_deriv = h**(-2) * (x[:-2] - 2.0 * x[1:-1] + x[2:])
+        # I've set an option for whether to use the forward or the backward numerical first derivative.
+        # I don't believe this will make any difference, but it is there to allow testing / experimentation.
+        if first_deriv_fwd_mode:
+            first_deriv = h**(-1) * (x[2:] - x[1:-1])
+        else:
+            first_deriv = h**(-1) * (x[1:-1] - x[:-2])
+        rhs_val = rhs(x[1:-1], first_deriv, t_grid[1:-1])
+        # Note the factor of h. This cancels out the implicit factor of n_grid from the sum.
+        # Alternatively, think of this loss as the (approximation to) the integral of the l2-violation of the ODE.
+        loss_val = 0.5 * h * torch.sum((second_deriv - rhs_val) ** 2)
+        return loss_val
+
+    # Optimization takes place below here
+
+    # Initialize logging history
+    log_scalars = []
+    log_grids = []
+
+    iterations = tqdm(range(N_iter)) if show_progress else range(N_iter)
+    for iteration in iterations:
+        w_ODE = get_w_ODE(iteration, N_iter)
+
+        # Pass forward through the network
+        loss_data_torch = loss_data(z_solution_grid)
+        loss_ODE_torch = loss_ODE(z_solution_grid)
+        loss_total_torch = (1.0 - w_ODE) * loss_data_torch + w_ODE * loss_ODE_torch
+
+        # Store these for logging
+        loss_val_data = loss_data_torch.detach().item()
+        loss_val_ODE = loss_ODE_torch.detach().item()
+        loss_val_total = loss_total_torch.detach().item()
+
+        # Step the optimizer, updating z_solution_grid.
+        optimizer.zero_grad()
+        loss_total_torch.backward()
+        # Stepping the LBFGS optimizer requires a closure for evaluating the loss function
+        if type(optimizer) is torch.optim.LBFGS:
+            optimizer.step(lambda: (1.0 - w_ODE) * loss_data(z_solution_grid) + w_ODE * loss_ODE(z_solution_grid))
+        else:
+            optimizer.step()
+
+        # In many of my early experiments, the solution became NaN due to numerical instability.
+        # If this happens, it is useful to fail at this point.
+        # It is also helpful to know which iteration this happened at.
+        if z_solution_grid.isnan().any().item():
+            error_msg = f'NaNs appeared in solution after iteration {iteration}'
+            raise ValueError(error_msg)
+
+        if iteration % logging_freq_scalars == 0:
+            log_scalars.append({
+                'iteration': iteration,
+                'w_ODE': w_ODE,
+                'loss_data': loss_val_data,
+                'loss_ODE': loss_val_ODE,
+                'loss_total': loss_val_total,
+            })
+
+        if iteration % logging_freq_grids == 0:
+            log_grids.append({
+                'iteration': iteration,
+                'z_grid': z_solution_grid.detach().numpy(),
+                'x_grid': (transformation_z2x @ z_solution_grid).detach().numpy(),
+            })
+
+    # Convert the solution back to x-space.
+    # This may already be stored in the logs (if logging_freq_grids divides N_iter).
+    x_solution_grid = (transformation_z2x @ z_solution_grid).detach().numpy()
+
+    return {
+        'x_solution_grid': x_solution_grid,
+        'log_scalars': log_scalars,
+        'log_grids': log_grids,
+    }
